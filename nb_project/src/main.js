519a71da2becd3f7470ec8d6c938a25596bc7d77
hhh <599098339@qq.com>
2020-11-20 09:29:45 +0800

    213123213

--- nb_project/src/main.js
+++ nb_project/src/main.js
@@ -4,11 +4,8 @@ import router from './router'
 
 Vue.config.productionTip = false
 console.log(66666);
-<<<<<<< HEAD
 console.log("aaaaaaaaa");
-=======
-console.log("文件修改")
->>>>>>> 604ae5157d03c46849566ba3c45c838d3fd98288
+console.log("文件修改123")
 new Vue({
   router,
   render: h => h(App)
